fcd23b960a9ca7b6d27435c298fd54331826bb77
Roger Cedeño <rcedeno@ec.krugercorp.com>
2024-07-26 10:34:18 -0500

    documentacion services

--- android/app/src/services/api.ts
+++ android/app/src/services/api.ts
@@ -6,7 +6,10 @@ import {Product} from '../types/Product';
 const API_URL =
   'https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros';
 const AUTHOR_ID = '1723603997';
-
+/**
+ * Fetches products from the API.
+ * @returns {Promise<Product[]>}
+ */
 export const fetchProducts = async (): Promise<Product[]> => {
   try {
     const response = await axios.get(`${API_URL}/bp/products`, {
@@ -21,6 +24,11 @@ export const fetchProducts = async (): Promise<Product[]> => {
   }
 };
 
+/**
+ * Adds a product to the API.
+ * @param product
+ * @returns
+ */
 export const addProduct = async (product: Product) => {
   try {
     const response = await axios.post(`${API_URL}/bp/products`, product, {
@@ -36,6 +44,11 @@ export const addProduct = async (product: Product) => {
 };
 
 // Actualizar Producto Financiero
+/**
+ * Updates a product in the API.
+ * @param product
+ * @returns
+ */
 export const updateProduct = async (product: Product) => {
   try {
     const response = await axios.put(`${API_URL}/bp/products`, product, {
@@ -51,6 +64,11 @@ export const updateProduct = async (product: Product) => {
 };
 
 // Eliminar Producto Financiero
+/**
+ * Deletes a product from the API.
+ * @param productId
+ * @returns
+ */
 export const deleteProduct = async (productId: string) => {
   try {
     const response = await axios.delete(`${API_URL}/bp/products`, {
@@ -69,6 +87,11 @@ export const deleteProduct = async (productId: string) => {
 };
 
 // Verificar Existencia de ID
+/**
+ * Verifies if a product exists in the API.
+ * @param productId
+ * @returns
+ */
 export const verifyProductId = async (productId: string) => {
   try {
     const response = await axios.get(`${API_URL}/bp/products/verification`, {
